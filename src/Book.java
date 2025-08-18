05c1362b66b03ccab6f6faeff987290d1b71e775
BiancaDuarteCorreia <biancaduarte140109@gmail.com>
2025-08-18 09:05:33 -0300

    Atualização 09:05 18/08

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,9 @@
 public class Book {
     private String title;
     private String author;
+
+    public void setBook(String tiitle, String author){
+        this.title = tiitle;
+        this.author = author;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Manager manager = new Manager();
+        Book book = new Book();
         int opc;
+        List<String> books = new ArrayList<>();
 
         System.out.println("=======Administrador=======");
         System.out.print("Olá Adm! Digite o login do gerente: ");
@@ -21,25 +25,39 @@ public class Main {
         acessManagerPanel = Character.toUpperCase(acessManagerPanel);
 
         if (acessManagerPanel == 'S') {
-            System.out.print("Digite o login do gerente: ");
 
             do {
+                System.out.println("=======Gerente=======");
+                System.out.println("Digite 1 -> Cadastrar Livro");
+                System.out.println("Digite 2 -> Listar livros");
+                System.out.println("Digite 3 -> Alterar senha");
+                System.out.println("Digite 0 -> Encerrar programa");
 
+                System.out.print("Digite sua opção: ");
                 opc = sc.nextInt();
+
+                switch (opc){
+                    case 1:
+                        System.out.print("\n\nDigite o titulo do livro: ");
+                        break;
+                    case 2:
+                        break;
+                }
 
                 System.out.print("Olá! Digite o Login do gerente: ");
                 var loginManager = sc.next();
 
-                System.out.print("Olá! Digite o Login do gerente: ");
+                System.out.print("Olá! Digite a senha do gerente: ");
                 var passwordManager = sc.next();
 
                 if (manager.verifyLogin(loginManager, passwordManager)) {
                     System.out.println();
                 }
 
+                System.out.println("=====================");
             } while (opc != 0);
         } else{
-            System.out.println("Sistema encerrado.");
+            System.out.println("Sistema encerrando...");
         }
 
         System.out.print("===========================");
